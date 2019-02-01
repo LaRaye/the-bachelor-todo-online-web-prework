1b432f8b0df08a7930e6207ab7834cb97decd20f
LaRaye Johnson <lnj2105@gmail.com>
2019-02-01 19:55:43 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,13 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season.to_sym].each do |contestant_hash|
+    name_arr = [ ]
+    if contestant_hash.value?("Winner")
+      name_arr << (contestant_hash[:name]).split(" ")
+      name_arr[0]
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
@@ -17,3 +25,4 @@ end
 def get_average_age_for_season(data, season)
   # code here
 end
+
